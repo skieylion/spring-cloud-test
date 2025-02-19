54debfefaeb0ff944d7b718fc8615ae17d1b7f7e
e.ivannikov <e.ivannikov@itfbgroup.ru>
2025-02-19 21:16:04 +0300

    COnfig

--- router/src/main/java/org/example/router/FallbackController.java
+++ router/src/main/java/org/example/router/FallbackController.java
@@ -1,4 +1,15 @@
 package org.example.router;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/fallback")
 public class FallbackController {
+    @GetMapping
+    public Mono<String> fallback() {
+        return Mono.just("Сервис временно недоступен, попробуйте позже.");
+    }
 }
